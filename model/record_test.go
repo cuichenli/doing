1e3dc5b3e61efe8a6c5101d2ea984d80dc5142b3
Will Li <Will.Li@iress.com>
2020-05-28 21:55:04 +1000

    Add one unit test for converting record

--- model/record_test.go
+++ model/record_test.go
@@ -1,21 +1,28 @@
 package model_test
 
 import (
-	model "github.com/cuichenli/doing/model"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"time"
+
+	"github.com/cuichenli/doing/model"
 )
 
 var _ = Describe("Record", func() {
 	var (
 		record model.Record
+		loc    *time.Location
 	)
 
 	BeforeEach(func() {
+		loc, _ = time.LoadLocation("UTC")
 		record = model.Record{
-			Detail:      "dafas",
+			Detail:      "A simple task",
 			Status:      model.Doing,
-			CreatedTime: 1231321,
+			CreatedTime: time.Date(2015, 04, 03, 12, 20, 07, 27, loc),
 		}
+	})
+
+	It("Convert a basic record to task paper string", func() {
+		result := record.ToTaskPaper()
+		Expect(result).To(Equal("  - A simple task @created(2015-04-03T12:20:07Z)"))
 	})
 })
