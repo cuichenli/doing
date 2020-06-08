738bd2c16a065d779bca7577eda6dbeb1d19fae7
Will Li <Will.Li@iress.com>
2020-06-08 20:01:52 +1000

    Change struct for recordList and add recordId struct

--- model/record_list.go
+++ model/record_list.go
@@ -1,7 +1,20 @@
 package model
 
+// RecordID Data structure represents the Id for one record
+type RecordID struct {
+	ID int
+}
+
+// GetNextID Return the next ID to be used.
+func (recordId RecordID) GetNextID() RecordID {
+	return RecordID{ID: recordId.ID + 1}
+}
+
 // RecordList A list of doing records.
 type RecordList struct {
-	doneRecords  []Record
-	doingRecords []Record
+	doneRecords  map[RecordID]Record
+	doingRecords map[RecordID]Record
+	nextID       RecordID
+}
+
 }
