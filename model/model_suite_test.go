9076e439c42c8d9906275b169d64ef26e2e92166
Will Li <Will.Li@iress.com>
2020-06-08 21:52:22 +1000

    Add function to create dummy record for tests

--- model/model_suite_test.go
+++ model/model_suite_test.go
@@ -2,7 +2,9 @@ package model_test
 
 import (
 	"testing"
+	"time"
 
+	"github.com/cuichenli/doing/model"
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 )
@@ -12,8 +14,19 @@ var BeforeEach = ginkgo.BeforeEach
 var It = ginkgo.It
 var Expect = gomega.Expect
 var Equal = gomega.Equal
+var Context = ginkgo.Context
 
 func TestModel(t *testing.T) {
 	gomega.RegisterFailHandler(ginkgo.Fail)
 	ginkgo.RunSpecs(t, "Model Suite")
+}
+
+func CreateDummyRecord(detail string) model.Record {
+	loc, _ := time.LoadLocation("UTC")
+	dummyRecord := model.Record{
+		Status:      model.Doing,
+		CreatedTime: time.Date(2015, 04, 03, 12, 20, 07, 27, loc),
+		Detail:      detail,
+	}
+	return dummyRecord
 }
