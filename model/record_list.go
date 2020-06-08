5b1ffd97a709a9617b4481845bab3e755a2e3b27
Will Li <Will.Li@iress.com>
2020-06-08 20:21:54 +1000

    Add GetHashValue for recordId to be used as hash key

--- model/record_list.go
+++ model/record_list.go
@@ -10,15 +10,20 @@ func (recordId RecordID) GetNextID() RecordID {
 	return RecordID{ID: recordId.ID + 1}
 }
 
+// GetHashValue Get the record ID's hash key value
+func (recordId RecordID) GetHashValue() int {
+	return recordId.ID
+}
+
 // RecordList A list of doing records.
 type RecordList struct {
-	doneRecords  map[RecordID]Record
-	doingRecords map[RecordID]Record
+	doneRecords  map[int]Record
+	doingRecords map[int]Record
 	nextID       RecordID
 }
 
 // AddRecord Add one record to the record list.
 func (recordList *RecordList) AddRecord(record Record) {
-	recordList.doingRecords[recordList.nextID] = record
+	recordList.doingRecords[recordList.nextID.GetHashValue()] = record
 	recordList.nextID = recordList.nextID.GetNextID()
 }
