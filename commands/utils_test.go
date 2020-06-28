d1ce5a135e797aad4f4617d6dd9e1d9f45055230
Will Li <Will.Li@iress.com>
2020-06-28 21:45:53 +1000

    Rename test file

--- commands/utils_test.go
+++ commands/utils_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func TestAddErrorWhenGetExistingRecords(t *testing.T) {
+func TestAddDoingGetErrorWhenGetExistingRecords(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	oldGetExistingRecords := GetExistingRecords
 	defer func() {
@@ -22,11 +22,11 @@ func TestAddErrorWhenGetExistingRecords(t *testing.T) {
 	GetExistingRecords = func() (model.RecordList, error) {
 		return model.NewRecordList(), errors.New("Error on GetExistingRecords")
 	}
-	err := Add(&cobra.Command{}, []string{"record"})
+	err := add(&cobra.Command{}, []string{"record"})
 	g.Expect(err.Error()).Should(gomega.Equal("Error on GetExistingRecords"))
 }
 
-func TestAddWhenFailedToOpenFile(t *testing.T) {
+func TestAddDoingWhenFailedToOpenFile(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	oldGetExistingRecords := GetExistingRecords
 	oldOpenFile := openFile
@@ -43,12 +43,12 @@ func TestAddWhenFailedToOpenFile(t *testing.T) {
 	openFile = func(name string, flag int, perm os.FileMode) (*os.File, error) {
 		return &os.File{}, errors.New("Error on openning file")
 	}
-	err := Add(&cobra.Command{}, []string{"record"})
+	err := add(&cobra.Command{}, []string{"record"})
 	g.Expect(err.Error()).Should(gomega.Equal("Error on openning file"))
 	g.Expect(getExistingRecordsCount).To(gomega.Equal(1))
 }
 
-func TestAdd(t *testing.T) {
+func TestAddDoing(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	oldGetExistingRecords := GetExistingRecords
 	oldOpenFile := openFile
@@ -72,7 +72,40 @@ func TestAdd(t *testing.T) {
 	openFile = func(name string, flag int, perm os.FileMode) (*os.File, error) {
 		return &os.File{}, nil
 	}
-	err := Add(&cobra.Command{}, []string{"record"})
+	err := add(&cobra.Command{}, []string{"record"})
 	g.Expect(err).To(gomega.BeNil())
 	g.Expect(strings.HasPrefix(buf.String(), "  - record @created(")).To(gomega.BeTrue())
+}
+
+func TestAddDone(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	oldGetExistingRecords := GetExistingRecords
+	oldOpenFile := openFile
+	oldNewRecordsWriter := newRecordsWriter
+	defer func() {
+		GetExistingRecords = oldGetExistingRecords
+		openFile = oldOpenFile
+		newRecordsWriter = oldNewRecordsWriter
+	}()
+	GetExistingRecords = func() (model.RecordList, error) {
+		return model.NewRecordList(), nil
+	}
+	var buf bytes.Buffer
+	newRecordsWriter = func(file string, writer io.Writer) model.RecordsWriter {
+		return model.RecordsWriter{
+			Writer:     &buf,
+			ConfigFile: file,
+		}
+	}
+
+	openFile = func(name string, flag int, perm os.FileMode) (*os.File, error) {
+		return &os.File{}, nil
+	}
+	err := done(&cobra.Command{}, []string{"record"})
+	g.Expect(err).To(gomega.BeNil())
+	output := buf.String()
+	g.Expect(strings.HasPrefix(output, "  - record @created(")).To(gomega.BeTrue())
+	// Remove the trailing new line symbol
+	output = strings.TrimSpace(output)
+	g.Expect(strings.HasSuffix(output, "@done")).To(gomega.BeTrue())
 }
